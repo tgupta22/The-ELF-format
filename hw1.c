2204ca14f229fca8f0c173bcba7714418512423a
tgupta22 <44489499+tgupta22@users.noreply.github.com>
2020-06-09 14:07:34 -0500

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "hw1.h"
+
+
+
+int main(int argc,char* argv[]){
+//  printf("this is probably the worst skeleton code ever\n");
+ 
+       printf("tgupta22\n");
+	return 0;
+}
