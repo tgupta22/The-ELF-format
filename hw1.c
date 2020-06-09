3bde97a709041a88b102e9f577fe6951a4e7e9a7
tgupta22 <44489499+tgupta22@users.noreply.github.com>
2020-06-09 14:16:21 -0500

    Add files via upload

--- hw1.c
+++ hw1.c
@@ -3,9 +3,53 @@
 
 
 
-int main(int argc,char* argv[]){
-//  printf("this is probably the worst skeleton code ever\n");
- 
-       printf("tgupta22\n");
-	return 0;
+//int main(int argc,char* argv[]){
+  //printf("this is probably the worst skeleton code ever\n");
+  //return 0;
+  
+
+	static void I_have_written(){
+	printf ( "tgupta22");
 }
+
+	static void that_you_needed(){
+	}
+
+	static void and_which() {
+	}
+
+	static int  the_code[3] = {2,1,3};
+
+	
+
+	int main(int argc, char* argv[]){
+
+
+		I_have_written();
+		that_you_needed();
+		and_which();
+
+		static int to_compile = 2213;
+
+		static int has_a_bunch_of = 2220;
+
+		static int ridiculous = 2221;
+		static double symbols;
+
+		to_compile++;
+
+		has_a_bunch_of++;
+
+		ridiculous++;
+
+		symbols++;
+		the_code[1] = 2;
+		Forgive_me();
+
+		and_so_varied();
+
+
+
+		return 0;
+	}
+
---
+++ hw1.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+	void sides_and()
+{
+}
+	
+	void  Forgive_me()
+{
+}
+	int they_are_arbitrary;
+
+	double so_random = 6789;
+	
+	void and_so_varied()
+{
+}
+;
+
+
